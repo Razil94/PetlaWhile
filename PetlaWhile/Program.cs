8a511940d14c49067d6a58be1145dcc1f4eaf93c
Lukas <razilgry@gmail.com>
2023-12-09 06:54:14 +0100

    Dodaj pliki projektów.

---
+++ PetlaWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> values = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Podaj liczbę (0-10, wpisz 5 aby zakończyć): ");
+            if (!int.TryParse(Console.ReadLine(), out int userInput) || userInput < 0 || userInput > 10)
+            {
+                Console.WriteLine("Wprowadź liczbę z zakresu 0-10.");
+                continue;
+            }
+
+            if (userInput == 5)
+                break;
+
+            values.Add(userInput);
+        }
+
+        if (values.Count > 0)
+        {
+            int squaredSum = values.Sum(x => x * x);
+            int minimumValue = values.Min();
+
+            Console.WriteLine($"Suma kwadratów wprowadzonych liczb: {squaredSum}");
+            Console.WriteLine($"Najmniejsza wprowadzona liczba: {minimumValue}");
+        }
+        else
+        {
+            Console.WriteLine("Nie wprowadzono żadnych liczb.");
+        }
+    }
+}
